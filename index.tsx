f85bb70f23eb97c1ecb94a48b6bdb7987702e833
Mustapha ACHAOUD <achaoud.mustapha@gmail.com>
2025-10-18 18:45:38 +0100

    update 5 website version

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // ← Added this line to import Tailwind CSS
+import './index.css'; // ← IMPORTANT!
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
