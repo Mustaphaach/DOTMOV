0455164919a7980f740f60a591c857ddd2fdf19b
Mustapha ACHAOUD <achaoud.mustapha@gmail.com>
2025-10-22 12:22:29 +0100

    update 10 website version

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import React from 'react';
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header';
